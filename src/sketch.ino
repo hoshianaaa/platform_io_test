c9191d8f22dba742ef155df5faa2bd5065d4979d
hoshianaaa <hoshina54321@gmail.com>
2022-05-30 15:03:23 +0900

    add test code

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,18 +1,21 @@
 #define LED_PIN 13
+#include <calculator.h>
 
-    bool return_true()
-    {
-      return true;
-    }
+Calculator calc;
 
-    void setup() {
-        pinMode(LED_PIN, OUTPUT);
-        Serial.begin(9600);
-    }
+bool return_true()
+{
+  return true;
+}
 
-    void loop() {
-        return_true();
-        Serial.println("Hello Arduino");
-        digitalWrite(LED_PIN, HIGH); delay(3000);
-        digitalWrite(LED_PIN, LOW);  delay(3000);
-    }
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    Serial.println("Hello Arduino");
+    Serial.println(calc.add(25, 17));
+    digitalWrite(LED_PIN, HIGH); delay(3000);
+    digitalWrite(LED_PIN, LOW);  delay(3000);
+}
