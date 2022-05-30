cfe93cf7455f2ca19515a406df107c95bbe1f561
hoshianaaa <hoshina54321@gmail.com>
2022-05-30 14:32:19 +0900

    update

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,9 +1,17 @@
 #define LED_PIN 13
+
+    bool return_true()
+    {
+      return true;
+    }
+
     void setup() {
         pinMode(LED_PIN, OUTPUT);
         Serial.begin(9600);
     }
+
     void loop() {
+        return_true();
         Serial.println("Hello Arduino");
         digitalWrite(LED_PIN, HIGH); delay(3000);
         digitalWrite(LED_PIN, LOW);  delay(3000);
